7ff6de1901736e55933e91bc57e9d1e0e1311dea
slooi <s.looi2@outlook.com>
2020-04-22 00:47:22 +1200

    init commit

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log('main.js loaded')
+const textarea = document.getElementById('textarea')
+
+
+// LOAD DATA
+loadText()
+
+// after any change
+function saveText(){
+	// get values from textarea and put into localstorage
+	console.log('asd')
+	localStorage.setItem('text',textarea.value)
+}
+
+// on init
+function loadText(){
+	// get data
+	const text = localStorage.getItem('text')
+
+	// render data
+	textarea.value = text
+}
+
+function increaseLength(){
+	textarea.style.height = textarea.clientHeight+1000+'px'
+}
+
+
+textarea.addEventListener('keydown',e=>{
+	saveText()
+})
+textarea.addEventListener('keyup',e=>{
+	saveText()
+})
